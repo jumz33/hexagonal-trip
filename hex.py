e70e97a139eb7e738f3507c49bd523fdb7ec62a8
Zakhar <73907441+jumz33@users.noreply.github.com>
2021-01-04 03:30:13 +0300

    Add files via upload

--- hex.py
+++ hex.py
@@ -1,57 +1,46 @@
-'''
-    "Hexagonal trip"
-    Author: Zakhar "Jummiuzz33"
-'''
-
 # f - move forward
 # l - rotate left
 # r - rotate right
-commands = input('What to do?\n> ')
+# example: fflffrrf
+command_sequence = input('? ')
 
-# [x, y, z]
-helicopter_pos = [0, 0, 0]
-truck_pos = [0, 0, 0]
+# x, y
+helicopter_pos = [0, 0]
+truck_pos = [0, 0]
 # rotation
-# 0 - 5 by faces
+# range from 0 to 5 
 helicopter_rt = 0
 truck_rt = 0
 
+# 1st face = [-1, 1], 2nd face = [-1, 1] and etc.
+faces = [
+    [-1, 1], [-1, 0], [0, -1],
+    [1, -1], [1, 0], [0, 1]
+]
+
 # helicopter
-for i in commands:
+for command in command_sequence:
     # rotate left
-    if i == 'l':
+    if command == 'l':
         helicopter_rt -= 1
         if helicopter_rt < 0:
             helicopter_rt += 6
     # rotate right
-    if i == 'r':
+    if command == 'r':
         helicopter_rt += 1
         if helicopter_rt == 6:
             helicopter_rt -= 6
     # move forward
-    if i == 'f':
-        def change_pos(x, y, z):
-            helicopter_pos[0] += x
-            helicopter_pos[1] += y
-            helicopter_pos[2] += z
-        if helicopter_rt == 0:
-            change_pos(-1, 1, 0)
-        if helicopter_rt == 1:
-            change_pos(-1, 0, -1)
-        if helicopter_rt == 2:
-            change_pos(0, -1, -1)
-        if helicopter_rt == 3:
-            change_pos(1, -1, 0)
-        if helicopter_rt == 4:
-            change_pos(1, 0, 1)
-        if helicopter_rt == 5:
-            change_pos(0, 1, 1)
+    if command == 'f':
+        for axis in range(2):
+            helicopter_pos[axis] += faces[helicopter_rt][axis]
 print(f'Helicopter position: {helicopter_pos}')
 
 # truck
+print('> ', end='')
 while truck_pos != helicopter_pos:
-    # change position
-    pos_before = truck_pos[0], truck_pos[1], truck_pos[2]
+    # POSITION
+    truck_pos_before = truck_pos[0], truck_pos[1]
     # x axis
     if truck_pos[0] != helicopter_pos[0]:
         x1 = truck_pos[0] + 1
@@ -80,12 +69,56 @@ while truck_pos != helicopter_pos:
                 truck_pos[1] += 1
             else:
                 truck_pos[1] -= 1
-    # can't do x +- 1 and y +- 1 at the same time
-    if truck_pos[1] - pos_before[1] == truck_pos[0] - pos_before[0]:
-        truck_pos[1] = pos_before[1]
-    # z axis
-    # z = x + y
-    truck_pos[2] = truck_pos[0] + truck_pos[1]
+    # cannot change x and y at the same time
+    if truck_pos[1] - truck_pos_before[1] == truck_pos[0] - truck_pos_before[0]:
+        # change only x
+        try:
+            truck_rt_now_1 = truck_rt - faces.index([truck_pos[0], truck_pos_before[1]])
+            if truck_rt_now_1 < 0:
+                truck_rt_now_1 += 6
+            #
+            if truck_rt_now_1 == 4:
+                truck_rt_now_1 = 2
+            if truck_rt_now_1 == 5:
+                truck_rt_now_1 = 1
+        except ValueError:
+            truck_rt_now_1 = 6
+        # change only y
+        try:
+            truck_rt_now_2 = truck_rt - faces.index([truck_pos_before[0], truck_pos[1]])
+            if truck_rt_now_2 < 0:
+                truck_rt_now_2 += 6
+            #
+            if truck_rt_now_2 == 4:
+                truck_rt_now_2 = 2
+            if truck_rt_now_2 == 5:
+                truck_rt_now_2 = 1
+        except ValueError:
+            truck_rt_now_2 = 6
+        # make a choice
+        if truck_rt_now_1 > truck_rt_now_2:
+            truck_pos[0] = truck_pos_before[0]
+        else:
+            truck_pos[1] = truck_pos_before[1]
+    # ROTATION
+    truck_pos_changes = [0, 0]
+    for i in range(2):
+        truck_pos_changes[i] = truck_pos[i] - truck_pos_before[i]
+    truck_rt -= faces.index(truck_pos_changes)
+    if truck_rt < 0:
+        truck_rt += 6
+    while truck_rt != 0:
+        if truck_rt <= 3:
+            truck_rt -= 1
+            print('l', end='')
+        if 3 < truck_rt:
+            truck_rt += 1
+            if truck_rt == 6:
+                truck_rt = 0
+            print('r', end='')
+    print('f', end='')
+    truck_rt += faces.index(truck_pos_changes)
+    if truck_rt > 5:
+        truck_rt -= 6
+print('.')
 
-    print(f'go to -> {truck_pos}')
-print('Finish')
